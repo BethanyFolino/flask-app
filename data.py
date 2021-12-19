13f2db51485775597625e9200fef882a10eb25d6
BethanyFolino <bethany.folino@gmail.com>
2021-12-19 12:25:04 -0800

    Completed Python Flask from Scratch part 1, started app.py; made templates for navbar, about, article, articles, home, layout; made data.py for article data

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis ducimus praesentium ullam perspiciatis aut incidunt voluptatum? Harum praesentium voluptas unde repellendus facilis et at. Vel optio quaerat aperiam veritatis consectetur.',
+            'author': 'Bethany Folino',
+            'create_date': '12-19-2021'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis ducimus praesentium ullam perspiciatis aut incidunt voluptatum? Harum praesentium voluptas unde repellendus facilis et at. Vel optio quaerat aperiam veritatis consectetur.',
+            'author': 'Jane Doe',
+            'create_date': '12-19-2021'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis ducimus praesentium ullam perspiciatis aut incidunt voluptatum? Harum praesentium voluptas unde repellendus facilis et at. Vel optio quaerat aperiam veritatis consectetur.',
+            'author': 'Bethany Folino',
+            'create_date': '12-19-2021'
+        }
+    ]
+    return articles
